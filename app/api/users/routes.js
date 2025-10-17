eb9e0d316f68b9eb198329adb864e0dce6bd4710
gaurav-pync <gaurav.t@pync.in>
2025-10-17 19:08:23 +0530

    first commit

---
+++ app/api/users/routes.js
@@ -0,0 +1,13 @@
+// app/api/users/route.js
+// import {NextResponse} from 'next/server';
+
+export async function GET() {
+  const users = [
+    { id: 1, name: "Aisha" },
+    { id: 2, name: "Ravi" },
+    { id: 3, name: "Sara" },
+  ];
+
+  // Return as JSON
+  return Response.json(users);
+}
